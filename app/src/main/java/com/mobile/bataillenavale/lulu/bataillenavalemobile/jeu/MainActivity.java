d26dfb946aa100966c5211d04d3f09782e4490cf
simon <thevret.simon@gmail.com>
2017-12-19 16:59:41 +0100

    blablalba ajout drag and drop a adapter

---
+++ app/src/main/java/com/mobile/bataillenavale/lulu/bataillenavalemobile/jeu/MainActivity.java
@@ -0,0 +1,183 @@
+package com.mobile.bataillenavale.lulu.bataillenavalemobile.jeu;
+
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipDescription;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.DragEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.plateau2);
+        final View star = findViewById(R.id.star);
+        star.setTag("STAR");
+        star.setOnLongClickListener(new View.OnLongClickListener() {
+
+            // Defines the one method for the interface, which is called when the View is long-clicked
+            public boolean onLongClick(View v) {
+
+                // Create a new ClipData.
+                // This is done in two steps to provide clarity. The convenience method
+                // ClipData.newPlainText() can create a plain text ClipData in one step.
+
+                // Create a new ClipData.Item from the ImageView object's tag
+                // ClipData.Item item = new ClipData.Item((String)v.getTag());
+
+                // Create a new ClipData using the tag as a label, the plain text MIME type, and
+                // the already-created item. This will create a new ClipDescription object within the
+                // ClipData, and set its MIME type entry to "text/plain"
+                ClipData dragData = ClipData.newPlainText(String.valueOf(v.getId()), String.valueOf(v.getId()));
+
+                // Instantiates the drag shadow builder.
+                View.DragShadowBuilder myShadow = new View.DragShadowBuilder(star);
+
+                // Starts the drag
+
+                v.startDrag(dragData,  // the data to be dragged
+                        myShadow,  // the drag shadow builder
+                        null,      // no need to use local data
+                        0          // flags (not currently used, set to 0)
+                );
+                return true;
+            }
+
+        });
+        Plateau p = new Plateau(5,5,this);
+        LinearLayout v = (LinearLayout) findViewById(R.id.main);
+        v.addView(p.getTable(),0,new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT,1));
+        v.invalidate();
+    }
+
+    public myDragEventListener getDragEvent(){
+        return new myDragEventListener();
+    }
+
+    public class myDragEventListener implements View.OnDragListener {
+
+        private boolean recus = false;
+
+        // This is the method that the system calls when it dispatches a drag event to the
+        // listener.
+        public boolean onDrag(View v, DragEvent event) {
+
+            // Defines a variable to store the action type for the incoming event
+            final int action = event.getAction();
+
+            // Handles each of the expected events
+            switch(action) {
+
+                case DragEvent.ACTION_DRAG_STARTED:
+
+                    // Determines if this View can accept the dragged data
+                    if (event.getClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
+
+                        // As an example of what your application might do,
+                        // applies a blue color tint to the View to indicate that it can accept
+                        // data.
+                        v.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+
+                        // Invalidate the view to force a redraw in the new tint
+                        v.invalidate();
+                        recus = false;
+                        // returns true to indicate that the View can accept the dragged data.
+                        return true;
+
+                    }
+
+                    // Returns false. During the current drag and drop operation, this View will
+                    // not receive events again until ACTION_DRAG_ENDED is sent.
+                    return false;
+
+                case DragEvent.ACTION_DRAG_ENTERED:
+
+                    // Applies a green tint to the View. Return true; the return value is ignored.
+                    v.setBackgroundTintList(ColorStateList.valueOf(Color.BLUE));
+
+                    // Invalidate the view to force a redraw in the new tint
+                    v.invalidate();
+
+                    return true;
+
+                case DragEvent.ACTION_DRAG_LOCATION:
+
+                    // Ignore the event
+                    return true;
+
+                case DragEvent.ACTION_DRAG_EXITED:
+
+                    // Re-sets the color tint to blue. Returns true; the return value is ignored.
+                    v.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+
+                    // Invalidate the view to force a redraw in the new tint
+                    v.invalidate();
+
+                    return true;
+
+                case DragEvent.ACTION_DROP:
+
+                    // Gets the item containing the dragged data
+                    ClipData.Item item = event.getClipData().getItemAt(0);
+
+                    // Gets the text data from the item.
+                    CharSequence dragData = item.getText();
+
+                    // Displays a message containing the dragged data.
+                    Toast.makeText(getApplicationContext(), "Dragged data is " + dragData, Toast.LENGTH_LONG).show();
+
+                    // Turns off any color tints
+                    v.setBackgroundTintList(ColorStateList.valueOf(Color.MAGENTA));
+                    recus = true;
+                    RelativeLayout l = (RelativeLayout) v;
+                    View b = findViewById(Integer.parseInt((String)dragData));
+                    RelativeLayout pool =(RelativeLayout) findViewById(((View)b.getParent()).getId());
+                    pool.removeView(b);
+                    l.addView(b);
+                    // Invalidates the view to force a redraw
+                    v.invalidate();
+
+                    // Returns true. DragEvent.getResult() will return true.
+                    return true;
+
+                case DragEvent.ACTION_DRAG_ENDED:
+
+                    // Turns off any color tinting
+                    if(!recus)
+                        v.setBackgroundTintList(null);
+
+                    // Invalidates the view to force a redraw
+                    v.invalidate();
+
+                    // Does a getResult(), and displays what happened.
+                    if (event.getResult()) {
+                        Toast.makeText(getApplicationContext(), "The drop was handled.", Toast.LENGTH_LONG).show();
+
+                    } else {
+                        Toast.makeText(getApplicationContext(), "The drop didn't work.", Toast.LENGTH_LONG).show();
+
+                    }
+
+                    // returns true; the value is ignored.
+                    return true;
+
+                // An unknown action type was received.
+                default:
+                    Log.e("DragDrop Example","Unknown action type received by OnDragListener.");
+                    break;
+            }
+
+            return false;
+        }
+    };
+}
---
+++ app/src/main/java/com/mobile/bataillenavale/lulu/bataillenavalemobile/jeu/Plateau.java
@@ -0,0 +1,50 @@
+package com.mobile.bataillenavale.lulu.bataillenavalemobile.jeu;
+
+import android.graphics.Color;
+import android.view.Gravity;
+import android.widget.RelativeLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+
+/**
+ * Created by Simon on 18/12/2017.
+ */
+
+public class Plateau {
+    private TableLayout table;
+    static private int X = 1;
+    static private int Y = 1;
+
+    public Plateau(int x, int y, MainActivity activity){
+        if(x<0 || y<0)
+            throw new IllegalArgumentException("taille negative");
+        if(x>99)
+            throw new IllegalArgumentException("longeur > 99");
+        table =new TableLayout(activity.getApplicationContext());
+        //table.setBackgroundColor(Color.MAGENTA);
+        for(int yi=0;yi<y;yi++) {
+            TableRow row = new TableRow(activity.getApplicationContext());
+            row.setId(yi);
+            row.setGravity(Gravity.CENTER);
+            row.setBackgroundColor(Color.BLACK);
+            for (int xi = 0; xi < x; xi++) {
+                RelativeLayout cell = new RelativeLayout(activity.getApplicationContext());
+                TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT);
+                row.addView(cell,xi,params);
+                cell.setBackgroundColor(Color.BLUE);
+                cell.setOnDragListener(activity.getDragEvent());
+                cell.setGravity(Gravity.CENTER);
+                //implique que xi < 100
+                cell.setId(100*yi+xi);
+                cell.setTag(X,xi);
+                cell.setTag(Y,yi);
+            }
+            table.addView(row,new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.MATCH_PARENT,1f));
+        }
+        table.setStretchAllColumns(true);
+    }
+
+    public TableLayout getTable(){
+        return table;
+    }
+}
