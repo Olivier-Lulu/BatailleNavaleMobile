3ea2668f4cebd4829448cf28672466b9f4fd7712
simon <thevret.simon@gmail.com>
2017-12-20 17:10:37 +0100

    modif plateau

--- app/src/main/java/com/mobile/bataillenavale/lulu/bataillenavalemobile/jeu/MainActivity.java
+++ app/src/main/java/com/mobile/bataillenavale/lulu/bataillenavalemobile/jeu/MainActivity.java
@@ -15,7 +15,7 @@ import android.widget.Toast;
 
 import com.mobile.bataillenavale.lulu.bataillenavalemobile.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements Controleur {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,130 +54,16 @@ public class MainActivity extends Activity {
             }
 
         });
-        Plateau p = new Plateau(5,5,this);
-        LinearLayout v = (LinearLayout) findViewById(R.id.main);
-        v.addView(p.getTable(),0,new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT,1));
-        v.invalidate();
+        Plateau p = new Plateau(5,5,this,this);
     }
 
-    public myDragEventListener getDragEvent(){
-        return new myDragEventListener();
+    @Override
+    public boolean canHoastBoat(int x, int y) {
+        return false;
     }
 
-    public class myDragEventListener implements View.OnDragListener {
+    @Override
+    public void obtaineBoat(CharSequence boatId, int xCell, int yCell) {
 
-        private boolean recus = false;
-
-        // This is the method that the system calls when it dispatches a drag event to the
-        // listener.
-        public boolean onDrag(View v, DragEvent event) {
-
-            // Defines a variable to store the action type for the incoming event
-            final int action = event.getAction();
-
-            // Handles each of the expected events
-            switch(action) {
-
-                case DragEvent.ACTION_DRAG_STARTED:
-
-                    // Determines if this View can accept the dragged data
-                    if (event.getClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
-
-                        // As an example of what your application might do,
-                        // applies a blue color tint to the View to indicate that it can accept
-                        // data.
-                        v.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
-
-                        // Invalidate the view to force a redraw in the new tint
-                        v.invalidate();
-                        recus = false;
-                        // returns true to indicate that the View can accept the dragged data.
-                        return true;
-
-                    }
-
-                    // Returns false. During the current drag and drop operation, this View will
-                    // not receive events again until ACTION_DRAG_ENDED is sent.
-                    return false;
-
-                case DragEvent.ACTION_DRAG_ENTERED:
-
-                    // Applies a green tint to the View. Return true; the return value is ignored.
-                    v.setBackgroundTintList(ColorStateList.valueOf(Color.BLUE));
-
-                    // Invalidate the view to force a redraw in the new tint
-                    v.invalidate();
-
-                    return true;
-
-                case DragEvent.ACTION_DRAG_LOCATION:
-
-                    // Ignore the event
-                    return true;
-
-                case DragEvent.ACTION_DRAG_EXITED:
-
-                    // Re-sets the color tint to blue. Returns true; the return value is ignored.
-                    v.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
-
-                    // Invalidate the view to force a redraw in the new tint
-                    v.invalidate();
-
-                    return true;
-
-                case DragEvent.ACTION_DROP:
-
-                    // Gets the item containing the dragged data
-                    ClipData.Item item = event.getClipData().getItemAt(0);
-
-                    // Gets the text data from the item.
-                    CharSequence dragData = item.getText();
-
-                    // Displays a message containing the dragged data.
-                    Toast.makeText(getApplicationContext(), "Dragged data is " + dragData, Toast.LENGTH_LONG).show();
-
-                    // Turns off any color tints
-                    v.setBackgroundTintList(ColorStateList.valueOf(Color.MAGENTA));
-                    recus = true;
-                    RelativeLayout l = (RelativeLayout) v;
-                    View b = findViewById(Integer.parseInt((String)dragData));
-                    RelativeLayout pool = (RelativeLayout) findViewById(((View)b.getParent()).getId());
-                    pool.removeView(b);
-                    l.addView(b);
-                    // Invalidates the view to force a redraw
-                    v.invalidate();
-
-                    // Returns true. DragEvent.getResult() will return true.
-                    return true;
-
-                case DragEvent.ACTION_DRAG_ENDED:
-
-                    // Turns off any color tinting
-                    if(!recus)
-                        v.setBackgroundTintList(null);
-
-                    // Invalidates the view to force a redraw
-                    v.invalidate();
-
-                    // Does a getResult(), and displays what happened.
-                    if (event.getResult()) {
-                        Toast.makeText(getApplicationContext(), "The drop was handled.", Toast.LENGTH_LONG).show();
-
-                    } else {
-                        Toast.makeText(getApplicationContext(), "The drop didn't work.", Toast.LENGTH_LONG).show();
-
-                    }
-
-                    // returns true; the value is ignored.
-                    return true;
-
-                // An unknown action type was received.
-                default:
-                    Log.e("DragDrop Example","Unknown action type received by OnDragListener.");
-                    break;
-            }
-
-            return false;
-        }
-    };
+    }
 }
--- app/src/main/java/com/mobile/bataillenavale/lulu/bataillenavalemobile/jeu/Plateau.java
+++ app/src/main/java/com/mobile/bataillenavale/lulu/bataillenavalemobile/jeu/Plateau.java
@@ -1,7 +1,11 @@
 package com.mobile.bataillenavale.lulu.bataillenavalemobile.jeu;
 
+import android.content.ClipData;
+import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.view.DragEvent;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -13,40 +17,91 @@ import com.mobile.bataillenavale.lulu.bataillenavalemobile.R;
  */
 
 public class Plateau {
-    private TableLayout table;
-    static private int X = 1;
-    static private int Y = 2;
+    private View[][] cells;
+    private Controleur controleur;
 
-    public Plateau(int x, int y, MainActivity activity){
+    public Plateau(int x, int y, MainActivity activity,Controleur controleur){
         if(x<0 || y<0)
             throw new IllegalArgumentException("taille negative");
         if(x>99)
             throw new IllegalArgumentException("longeur > 99");
-        table =new TableLayout(activity.getApplicationContext());
-        //table.setBackgroundColor(Color.MAGENTA);
+        this.controleur = controleur;
+        TableLayout table = (TableLayout) activity.findViewById(R.id.table);
+        cells = new View[x][y];
         for(int yi=0;yi<y;yi++) {
             TableRow row = new TableRow(activity.getApplicationContext());
             row.setId(yi);
             row.setGravity(Gravity.CENTER);
-            row.setBackgroundColor(Color.BLACK);
             for (int xi = 0; xi < x; xi++) {
                 RelativeLayout cell = new RelativeLayout(activity.getApplicationContext());
                 TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT);
                 row.addView(cell,xi,params);
-                cell.setBackgroundColor(Color.BLUE);
-                cell.setOnDragListener(activity.getDragEvent());
+                cell.setOnDragListener(new dragBoat());
                 cell.setGravity(Gravity.CENTER);
+                cell.setBackgroundColor(Color.TRANSPARENT);
                 //implique que xi < 100
                 cell.setId(100*yi+xi);
                 cell.setTag(R.id.X,xi);
                 cell.setTag(R.id.Y,yi);
+                cells[xi][yi] = cell;
             }
             table.addView(row,new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.MATCH_PARENT,1f));
         }
-        table.setStretchAllColumns(true);
+        table.invalidate();
     }
 
-    public TableLayout getTable(){
-        return table;
+    public class dragBoat implements View.OnDragListener {
+
+        public boolean onDrag(View v, DragEvent event) {
+
+            final int action = event.getAction();
+
+            switch(action) {
+
+                case DragEvent.ACTION_DRAG_STARTED:
+
+                    //on ne drag qu'un type de truc donc on a juste a tester si la cas est deja pris
+                    if (controleur.canHoastBoat((int)v.getTag(R.id.X),(int)v.getTag(R.id.Y))) {
+                        //la case peut accepter un bateau, ajoute un liserait vert
+                        //v.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+                        v.setBackgroundColor(Color.GREEN);
+                        v.invalidate();
+                        return true;
+                    }else{
+                        //la case n'accepte pas , ajout d'un liserait rouge
+                        v.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
+                        v.invalidate();
+                        return false;
+                    }
+
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    // ajout changement en vert plus foncer pour marquer que l'on est bien dans la case
+                    v.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+                    v.invalidate();
+                    return true;
+
+                case DragEvent.ACTION_DRAG_LOCATION:
+                    return true;
+
+                case DragEvent.ACTION_DRAG_EXITED:
+                    v.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+                    v.invalidate();
+                    return true;
+
+                case DragEvent.ACTION_DROP:
+                    ClipData.Item item = event.getClipData().getItemAt(0);
+                    CharSequence dragData = item.getText();
+                    controleur.obtaineBoat(dragData,(int)v.getTag(R.id.X),(int)v.getTag(R.id.X));
+                    return true;
+
+                case DragEvent.ACTION_DRAG_ENDED:
+                    v.setBackgroundTintList(null);
+                    v.invalidate();
+                    return true;
+
+                default:
+                    throw new IllegalArgumentException("DragEvent inconnue");
+            }
+        }
     }
 }
